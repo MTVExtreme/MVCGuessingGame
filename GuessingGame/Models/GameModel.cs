4ddf9e2eaeb08462e49d8119d2b92e5c39fa52de
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-10 13:18:19 -0400

    Added Game MVC

---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+        public string PlayerName { get; set; }
+
+
+        public int Guess { get; set; }
+
+    }
+}
