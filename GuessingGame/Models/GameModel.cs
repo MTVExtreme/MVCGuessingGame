648dcbd163b37af7041e41bcfd05b5e191e13823
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-10 14:23:09 -0400

    Fixed the Divs

--- GuessingGame/Models/GameModel.cs
+++ GuessingGame/Models/GameModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,11 @@ namespace GuessingGame.Models
 {
     public class GameModel
     {
+
+        [Display(Name = "Enter your name")]
         public string PlayerName { get; set; }
 
-
+        [Display(Name = "What is your guess?")]
         public int Guess { get; set; }
 
     }
